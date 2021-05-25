c5f70f475eb6d9e095c8bfb3980ffec01910ecd4
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-05-25 17:46:25 -0500

    Creadas clases Producto y Categoria

---
+++ Models/Producto.cs
@@ -0,0 +1,15 @@
+namespace BuscoApp.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string NombreP { get; set; }
+        public string Foto { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Celular { get; set; }
+        public string LugarCompra { get; set; }
+        public string NomComprador { get; set; }
+        public Categoria Categoria { get; set;}
+    }
+}
